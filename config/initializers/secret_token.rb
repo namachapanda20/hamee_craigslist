8c1aa6aad3e10de6e24ce2562da1ddb7593f977b
manaka kai <manaka-20.lisardggy@apost.plala.or.jp>
2014-05-06 21:44:39 +0900

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HameeCraigslist::Application.config.secret_token = 'b94eef2df9f623145f6d57d8076424f865ed24a486431c758eb6e881665835829c3177ad11e6fe5964d3a51658ad297647912932a8d5fedce6fd4bd8ecba2c6b'
